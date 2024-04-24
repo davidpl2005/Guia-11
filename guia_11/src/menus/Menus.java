c82b61ea6e685e583030a8667712f550b5d56113
Isac pacheco <isac.pachecolopez@unicolombo.edu.co>
2024-04-24 15:28:01 -0500

    Menus

---
+++ guia_11/src/menus/Menus.java
@@ -0,0 +1,21 @@
+
+package menus;
+
+/**
+ *
+ * @author isac pacheco
+ */
+public class Menus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal principal = new Principal();
+        
+        principal.setLocationRelativeTo(null);
+        principal.setVisible(true);
+    }
+    
+}
